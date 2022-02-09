66b8c89a1379f593b1035877afb4020c4fe65969
Lvetto <47479974+Lvetto@users.noreply.github.com>
2022-02-09 15:26:38 +0100

    riscritto con template

--- functions.h
+++ functions.h
@@ -1,17 +1,17 @@
 /*
 https://github.com/Lvetto/Esame-informatica
 */
-
+#include <cmath>
 #include <iostream>
 using namespace std;
-#define type point  // Permette di modificare il tipo degli elementi del vettore in un solo passaggio. Sostituire "point" con il nome della struttura da utilizzare (ad es: float)
+//#define type point  // Permette di modificare il tipo degli elementi del vettore in un solo passaggio.
 
-// Struct creato a scopo di esempio
 struct point {
-    float coords[3];
+    float x,y,w,m;
 };
 
 // Implementazione di una classe wrapper per un array di tipo "type" con supporto per numero di elementi variabile e alcune funzioni utili per domande di esame frequenti
+template <class type>
 class custom_vec {
     public:
         int len=0;  // Tiene traccia della lunghezza dell'array, inizialmente nulla
@@ -22,3 +22,5 @@ class custom_vec {
         void remove(int index); // Rimuove dall'array l'elemento di indice index e diminuisce la lunghezza di 1
         void sort(double (*func)(type), bool inverted); // Riordina in-place gli elementi dell'array secondo i valori double restituiti dalla funzione passata come parametro. Mettere inverted=true per ordine decrescente
 };
+
+double Fc(point p);
