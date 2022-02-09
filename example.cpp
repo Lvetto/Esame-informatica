30c426254174ee19f427e49ec44f7dacc31d5bb5
Lvetto <47479974+Lvetto@users.noreply.github.com>
2022-02-09 15:27:24 +0100

    riscritto con template

--- example.cpp
+++ example.cpp
@@ -1,44 +1,45 @@
 #include "functions.h"
-#include <cmath>
-
-/*
-Programma esempio scritto con la classe custom_vec
-*/
-
-// Distanza dall'origine, la funzione deve prendere un solo argomento di tipo type
-double distance(point p) {
-    return sqrt(pow(p.coords[0], 2) + pow(p.coords[0], 2) + pow(p.coords[0], 2));
-}
+#include <fstream>
 
 int main() {
-    // Dichiarazione istanza di custom_vec
-    custom_vec a;
+    fstream file;
+    file.open("punti.dat", ios::in);
+    if (!file.good()) throw runtime_error("Impossibile aprire il file");
 
-    // Inizializzazione vettore con valori pseudo-casuali
-    float b = 1.18;
-    for (int i=0; i<100; i++) {
-        point c;
-        c.coords[0] = b*3*i+7;
-        c.coords[1] = b*7*i+19;
-        c.coords[2] = b*4*i+12;
-        a.append(c);
+    custom_vec<point> punti;
+    while (true) {
+        if (file.eof()) break;
+        point t;
+        file >> t.x >> t.y >> t.w >> t.m;
+        punti.append(t);
     }
 
-    // Verifica che custom_vec.append() funzioni
-    for (int i=0; i<a.len; i++) cout << a.arr[i].coords[0] << " ";
-    cout << "\n";
+    cout << "Numero di punti: " << punti.len << endl;
+    for (int i=0; i<punti.len; i++)
+        cout << "Punto " << i+1 << ": " << punti.arr[i].x << " " << punti.arr[i].y << " " << punti.arr[i].w << " " << punti.arr[i].m << endl;
 
-    // Verifica che custom_vec.remove() funzioni
-    for (int i=0; i<a.len; i+=2) a.remove(i);
-    cout << "\n";
-    for (int i=0; i<a.len; i++) cout << a.arr[i].coords[0] << " ";
-    cout << "\n";
+    punti.sort(&Fc, false);
+    cout << "\n\n";
+    for (int i=0; i<punti.len; i++)
+        cout << "Punto " << i+1 << ": " << " " << punti.arr[i].w << " " << punti.arr[i].m << " " << Fc(punti.arr[i]) << endl;
 
-    // Verfica che custom_vec.sort() funzioni
-    a.sort(&distance, false);   // &distance è il puntatore alla funzione distance
-    cout << "\n";
-    for (int i=0; i<a.len; i++) cout << distance(a.arr[i]) << " ";
-    cout << "\n";
+    double Fm = 0;
+    for (int i=0; i<punti.len; i++)
+        Fm += (1./punti.len) * Fc(punti.arr[i]);
+    cout << "\n\nLa forza media è: " << Fm << endl;
+
+    custom_vec<point> punti_g;
+    custom_vec<point> punti_b;
+    for (int i=0; i<punti.len; i++) {
+        if (Fc(punti.arr[i]) < Fm*1.2)
+            punti_g.append(punti.arr[i]);
+        else
+            punti_b.append(punti.arr[i]);
+    }
+    cout << "\n\n" << "Numero di punti esclusi: " << punti_b.len << endl;
+    for (int i=0; i<punti_b.len; i++)
+        cout << "Punto " << i+1 << ": " << " " << punti_b.arr[i].w << " " << punti_b.arr[i].m << " " << Fc(punti_b.arr[i]) << endl;
 
     return 0;
 }
+
